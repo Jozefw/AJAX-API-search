740a31a36a57f91f2a419b6ac13b904e478ed496
Jozef <jozefw@sbcglobal.net>
2015-04-22 15:51:13 -0700

    comment the ajax call

--- main.js
+++ main.js
@@ -3,19 +3,20 @@ $('#commentForm').on('click', function(e){
 	$('#commentForm').validate();
 	console.log($('#movieTitle').val());
 	$.ajax({
+		// encodeURIComponent will escape spaces if the title has more than one word
 		url: 'http://www.omdbapi.com/', //encodeURIComponent($('#movieTitle').val()),
 		data: {
+// the data object takes all the keys provied in the API, and removes the ? query
 			t:$('#movieTitle').val(),
 		},
 		type: "GET",
 		dataType: "json",
 		success: function(response){
 		console.log(response);
-
 		},
 		error: function(xhr, status, error){
 			console.log({xhr:xhr,status:status, error:error});
-			console.log("not an objsect " ,xhr,status, error);
+			console.log("not an object " ,xhr,status, error);
 		},
 		complete: function(xhr,status){
 			console.log(xhr,status);
